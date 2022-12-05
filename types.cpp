008beff541e7c05f8b1bd148b890422f586908de
yonib98 <battat@campus.technion.ac.il>
2022-12-05 19:49:46 +0200

    Add symbols

--- types.cpp
+++ types.cpp
@@ -1,6 +1,71 @@
 #include "types.h"
+#include "hw3_output.hpp"
 #include <string>
 using namespace std;
-FuncNode::FuncNode(RetTypeNode return_type, string id, Node params){
-    table.top_scope().insert(Node*, );
+FuncNode::FuncNode(RetTypeNode return_type, string id, NodeParams* params){
+    FuncRecord func_record = FuncRecord(return_type.type, id, params);
+    table.insert_func(&func_record);
+}
+void SymTable::push_scope(){
+    scopes.push_back(Scope());
+    offsets.push_back(offsets.back()); //Copy last offset
+}
+Scope SymTable::top_scope(){
+    return scopes.back();
+}
+void SymTable::pop_scope(){
+    scopes.pop_back();
+    offset.pop_back();
+}
+int x;{
+    int x;
+}
+Record* SymTamble::get_record(IdNode id_n){
+    for (int i=scopes.size()-1;i>=0;i--){
+        Scope curr_scope = scopes[i];
+        for(int j=0;j<curr_scope.records.size();i++){
+            if(curr_scope.records[i].id == id_n.id){
+                return &(curr_scope.records[i]);
+            }
+        }
+    }
+    return nullptr;
+}
+
+void SymTable::check_symbol(IdNode id_n, TypeNode type_n, int lineno) {
+    Record* sym_record = get_record(id_n);
+    if(sym_record==nullptr){
+        output::errorUndef(lineno, id_n.id);
+        exit(0);
+    }
+    if (sym_record.type != type_n.type){
+        output::errorMismatch(lineno);
+        exit(0);
+    }
+
+}
+void SymTable::insert_symbol(TypeNode type_n, IdNode id_n, int lineno) {
+    if(get_record(id_n)!=nullptr){
+        output::errorDef(lineno, id_n.id);
+        exit(0);
+    }
+    int offset = offsets.back();
+    offsets.pop_back();
+    offsets.push_back(offset+1);
+    top_scope().insert_local_and_args_params(type_n.type,offset, id_n.id);
+}
+void SymTable::insert_and_check_symbol(TypeNode type_n, IdNode id_n, TypeNode exp_n, int lineno) {
+    if (type_n.type != exp_n.type){
+        output::errorMismatch(lineno);
+        exit(0);
+    }
+    insert_symbol(type_n, id_n, lineno);
+}
+void SymTable::create_func(TypeNode ret_type_n, IdNode id_n, NodeParams params_n, int lineno) {
+    if(get_record(id_n)!=nullptr){
+        output::errorDef(lineno, id_n.id);
+        exit(0);
+    }
+    top_scope().insert_func(ret_type_n.type, id_n.id, params);
+    table.push_scope();
 }
--- types.h
+++ types.h
@@ -10,64 +10,89 @@ enum Type{
     STRING,
     VOID,
 };
-struct Scope{
-    struct Record{
-        Node* var;
-        int offset;
+struct TypeNode {
+    Type type;
+    NodeParams* next;
+    NodeParams* back;
 
-        Record(Node* var, int offset): var(var), offset(offest){};
+public TypeNode(Type type): type(type), next(nullptr), back(nullptr){};
+};
+struct IdNode {
+    string id;
+public IdNode(string id): id(id){};
+};
+
+struct NodeParams{
+    Type type;
+    string id;
+    NodeParams* next;
+    NodeParams* back;
+public NodeParams(TypeNode type, IdNode id) type(type.type), id(id.id), next(nullptr), back(nullptr) {};
+};
+
+struct FuncCallNode{
+    IdNode id;
+    TypeNode types;
+    FuncCallNode(IdNode id, TypeNode types): id(id), types(types) {};
+    FuncCallNode(IdNode id) : id(id); {}
+};
+
+struct Scope{
+    //record
+    //1 -> local param -> offset +
+    //2 -> func arg -> offset -
+    //3 -> func -> offset = 0 (na) but has params and is_func == true
+    struct Record{
+        Type type;
+        int offset;
+        string id;
+        NodeParams* params_head;
+        bool is_func;
+        Record(Type type, int offset, string id): type(type), offset(offest), id(id), params_head(nullptr), is_func(
+                false){};
+        Record(Type type, string id, NodeParams* params_head): type(type), offset(0), id(id), params_head(params_head), is_func(true){};
     };
     vector<Record> records;
-    void insert(Node* var, int offset){
-        records.push_back(Record(var, offset));
+    void insert_local_param(Type type, int offset, string id){
+        records.push_back(Record(type, offset, id));
+    }
+    void insert_func(Type ret_type, string id, NodeParams* params){
+        records.push_back(Record(ret_type, id, params));
+    }
+    void push_params(NodeParams* params){
+        int offset = -1;
+        NodeParams* tmp = params;
+        while(tmp!= nullptr) {
+            Record func_arg = Record(tmp->type, offset, tmp->id);
+            offset--;
+            tmp = tmp->next
+        }
     }
 };
 struct SymTable{
     vector<Scope> scopes;
     vector<int> offsets;
-    void push_scope(){
-        scopes.push_back(Scope());
-        offsets.push_back(offsets.back()); //Copy last offset
-    }
-    Scope top_scope(){
-        return scopes.back();
-    }
-    void pop_scope(){
-        scopes.pop_back();
-        offset.pop_back();
-    }
-    void insert_symbol(Node* sym, int offset){
-        if FuncNode
-    }
+    void push_scope();
+    Scope top_scope();
+    void pop_scope();
+    void insert_symbol(TypeNode type_n, IdNode id_n, int lineno);
+    void check_symbol(IdNode id_n, TypeNode type_n, int lineno);
+    void insert_and_check_symbol(TypeNode type_n, IdNode id_n, TypeNode exp_n, int lineno);
+    void create_func(TypeNode return_type, IdNode id, NodeParams params,  int lineno);
+    bool check_func(FuncCallNode call_n);
+    void check_return();
+    void check_return(Type type);
+    void check_exp_is_bool(TypeNode type_n);
+    void check_break();
+    void check_continue();
+    void find_type_from_tri(TypeNode type_n1, TypeNode type_n2, TypeNode type_n3);
+    void find_type_from_add_min(TypeNode type_n1, TypeNode type_n2);
+    void find_type_from_mul_div(TypeNode type_n1, TypeNode type_n2);
+    void find_type_from_id(IdNode id_n);
+    void find_type_from_call_func(FuncCallNode call_n);
+    void end_program();
 };
 
 SymTable table;
-void createFunc(Node* return_type, Node* id, Node* params);
-struct TypeNode {
-    Type type;
-    int int_value;
-    string string_value;
-    bool bool_value;
-    char byte_value;
-public TypeNode(Type type): type(type){};
-};
-
-struct FuncNode  {
-    FuncNode(Node *return_type, Node *id, Node *params);
-};
-struct ProgramNode {};
-struct FuncsDeclNode {};
-struct RetTypeNode {
-    Type type;
-    RetTypeNode(Type type): type(type) {};`
-};
-struct FormalsNode {};
-struct FormalsListNode {};
-struct FormalDeclNode {};
-struct StatementsNode {};
-struct StatementNode {};
-struct CallNode {};
-struct ExpListNode {};
-struct ExpNodeNode {};
 
 
